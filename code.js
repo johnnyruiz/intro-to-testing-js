9d2e59489d014bba7afa4538a29062a729f12c44
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-04-05 14:28:10 -0500

    need to finish the exercises for homework

--- code.js
+++ code.js
@@ -3,5 +3,11 @@ function helloWorld() {
     return "Hello, World!";
 }
 function sayHello(a){
-    return "Hello, " + a + "!";
+     if (a === undefined) {
+        return "Hello, World!";
+    } else if (a === true){
+         return "Hello, World!";
+     } else {
+         return "Hello, " + a + "!";
+     }
 }
--- tests.js
+++ tests.js
@@ -24,6 +24,16 @@ describe('sayHello', function() {
     it('should return the string "Hello, Alex!" when executed', function() {
         expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
-});it('should return the string "Hello, Pat!" when executed', function() {
+    it('should return the string "Hello, Pat!" when executed', function() {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello()).toBe("Hello, World!");
+    });
+    it('should return the string "Hello,World!" when executed', function() {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World" when executed', function() {
+        expect(sayHello(false)).toBe("Hello, World!");
+    });
 });
